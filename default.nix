c24f25af9e4df91ef222af23e97b12cec5743b0e
Alberto Valverde <alberto@toscat.net>
2017-02-13 11:07:32 +0100

    initial commit

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, aeson, base, bytestring, http-api-data
+, insert-ordered-containers, lens, spatial-reference, stdenv
+, swagger2, text
+}:
+mkDerivation {
+  pname = "geohs-geometry";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bytestring http-api-data insert-ordered-containers lens
+    spatial-reference swagger2 text
+  ];
+  homepage = "https://github.com/GeoHS/geohs-geometry";
+  description = "GeoHS Geometry types";
+  license = stdenv.lib.licenses.bsd3;
+}
